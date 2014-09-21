1369dca0ec822a674b038cf6d2704d46e6c2c90e
Joel Coulson <joel@joelcoulson.com>
2014-09-21 21:35:15 +0800

    Updated with comments

--- src/com/joelcoulson/abstracts/Phone.java
+++ src/com/joelcoulson/abstracts/Phone.java
@@ -2,6 +2,10 @@ package com.joelcoulson.abstracts;
 
 import com.joelcoulson.interfaces.Describable;
 
+// here we implement the Describable interface which then
+// forces all classes implementing the Phone abstract class
+// to also implement the describe() method found in Describable
+
 public abstract class Phone implements Describable {
 
     public abstract void call(int number);
--- src/com/joelcoulson/abstracts/PhoneTest.java
+++ src/com/joelcoulson/abstracts/PhoneTest.java
@@ -4,6 +4,11 @@ public class PhoneTest {
 
     public static void main(String args[]) {
 
+        // here we create new LandLinePhone and MobilePhone objects
+        // using Phone references. we will use runtime polymorphism
+        // to determine which objects are to be used in the local
+        // call, answer and description classes
+
         Phone landline = new LandLinePhone();
         Phone mobile = new MobilePhone();
 
